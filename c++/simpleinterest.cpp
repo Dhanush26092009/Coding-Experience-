bf18d6eaaac155d8bada79464d411baa5c4566cb
Dhanush M <dhanush.manju26092009@gmail.com>
2026-07-04 15:28:00 +0530

    Simple interest

---
+++ c++/simpleinterest.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main (){
+     int  p ;
+    cout<<"Enter the principle amount : ";
+    cin>>p;
+    int t;
+    cout<<"enter the time duration : ";
+    cin>>t;
+    float r;
+    cout<<"enter the rate of interest ";
+    cin>>r;
+    cout<<p*t*r/100;
+    
+
+}
