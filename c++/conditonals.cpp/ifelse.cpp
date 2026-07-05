54a1745d048f906e2a55f942610420b52494e23f
Dhanush M <dhanush.manju26092009@gmail.com>
2026-07-05 20:44:40 +0530

    Ith Day of the Week

---
+++ c++/conditonals.cpp/ifelse.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int age;
+    cout<<"Enter the age: ";
+    cin>>age;
+    if (age>=18) cout<<"Eligible to drive: ";
+    else if (age<=0) cout<<"Invalid age:";
+    else cout<<"Not eligible to drive: ";
+}
