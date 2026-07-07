87835e9effc818900e1e17c80336c33fc5b05c64
Dhanush M <dhanush.manju26092009@gmail.com>
2026-07-07 20:43:57 +0530

    Clothing Suggestion Based on Temperature

---
+++ python/Clothing Suggestion Based on Temperature.py
@@ -0,0 +1,7 @@
+temp = float(input("Enter temperature in Celsius: "))
+if temp >= 30:
+    print("Wear light clothes.")
+elif 20 <= temp < 30:
+    print("Wear moderate clothing.")
+else:
+    print("Wear warm clothes.")
